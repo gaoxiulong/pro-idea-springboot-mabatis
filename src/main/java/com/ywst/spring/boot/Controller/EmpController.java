656f6f1bfa33fe5269b3d2256b7174f518bc3098
gaoxiulong <gaoxiulong139@163.com>
2021-01-21 15:29:36 +0800

    thymeleaf引入

--- src/main/java/com/ywst/spring/boot/Controller/EmpController.java
+++ src/main/java/com/ywst/spring/boot/Controller/EmpController.java
@@ -4,7 +4,7 @@ import com.ywst.spring.boot.entity.Emp;
 import com.ywst.spring.boot.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,7 +26,9 @@ public class EmpController {
 
     //springmvc框架，玩注解。跳转视图页面
     @RequestMapping("/common")
-    public String common(){
+    public String common(Model model){
+
+        model.addAttribute("attrName","Hello World!");
         return "common";
     }
 

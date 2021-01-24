60b5bc023daf9b537bac43660aefd4b1258ac055
gaoxiulong <gaoxiulong139@163.com>
2021-01-24 21:32:14 +0800

    thymeleaf优化

--- src/main/java/com/ywst/spring/boot/BootApplication.java
+++ src/main/java/com/ywst/spring/boot/BootApplication.java
@@ -18,6 +18,7 @@ public class BootApplication {
 
         System.out.println("系统已经成功启动！");
 
+
         //springboot打成war包步骤：
         //1.application修改如下代码：新加@ServletComponentScan注解，并且继承SpringBootServletInitializer
         //2.pom.xml修改为如下代码，主要两个改动：新加打包成war的声明： <packaging>war</packaging>
